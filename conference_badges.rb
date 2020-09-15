9e520d428ae0f32792809cc892c894e6b249dcdd
Erick Castille <ecastille924@gmail.com>
2020-09-15 22:34:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,12 @@
 speakers = ("Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz")
 
-def badge_maker (name)
+def badge_maker(name)
   "Hello, my name is #{name}"
 end 
 
 def batch_badge_creator (folks)
   badge_names = []
   folks.each do |person|
-    badge_maker (person)
+    badge_maker(person)
 end
 end
