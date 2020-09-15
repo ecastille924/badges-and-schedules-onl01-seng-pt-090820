e5ab49c7794cb4bab4f9fb54b89c1a06de6b6ef2
Erick Castille <ecastille924@gmail.com>
2020-09-15 22:42:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,8 +8,8 @@ def batch_badge_creator (people)
 end
 end
 
-def assign_rooms (people)
-  people.map.with_index do |person, index|
+def assign_rooms(people)
+  people.map.with_index(1) do |person, index|
     "Hello, #{person}! You'll be assigned to room #{index}!"
   end 
 end
